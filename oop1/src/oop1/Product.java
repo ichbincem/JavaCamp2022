c74f8c991d307c378bd1418bbcfcf3128a23abc8
ichbincem <109858248+ichbincem@users.noreply.github.com>
2022-09-29 04:17:59 +0300

    2. Hafta Kodları

---
+++ oop1/src/oop1/Product.java
@@ -0,0 +1,52 @@
+package oop1;
+
+//PascalCase
+public class Product {
+	private String name;
+	// camelCase
+	private double unitPrice;
+	private double discount;
+	private int unitsInStock;
+	private String imageUrl;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getUnitPrice() {
+		return unitPrice;
+	}
+
+	public void setUnitPrice(double unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+
+	public double getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(double discount) {
+		this.discount = discount;
+	}
+
+	public int getUnitsInStock() {
+		return unitsInStock;
+	}
+
+	public void setUnitsInStock(int unitsInStock) {
+		this.unitsInStock = unitsInStock;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+}
